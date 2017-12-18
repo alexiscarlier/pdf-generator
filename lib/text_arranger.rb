29400a59614cef9fdc41382084d12606e886e5ed
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-18 09:42:13 +0000

    Add text and separated text attributes, some refactoring

--- lib/text_arranger.rb
+++ lib/text_arranger.rb
@@ -1,24 +1,27 @@
 class TextArranger
 
-  attr_reader :text
+  attr_reader :text, :separated_text
 
   def initialize(text)
     @text = text
   end
 
   def separate
-    @text.split(/\n+/)
+    @separated_text = @text.split(/\n+/)
   end
 
-  def get_string_indexes(separated_text)
+  def get_string_indexes
     i = 0
-    strings_indexes = []
-    while i < separated_text.length
-      if separated_text[i][0] != '.'
-        strings_indexes.push(i)
+    string_indexes = []
+    while i < @separated_text.length
+      if @separated_text[i][0] != '.'
+        string_indexes.push(i)
       end
       i += 1
     end
-    strings_indexes
+    string_indexes
   end
+
+
+
 end
--- spec/text_arranger_spec.rb
+++ spec/text_arranger_spec.rb
@@ -15,14 +15,14 @@ very first'
 
   describe '#separate' do
   it 'separates the text based on what method calls they will receive' do
-    expect(text_arranger.separate()).to eq [".large", "My First Document", ".normal", ".paragraph", "This is my", ".italic", ".bold", "very first"]
+    expect(text_arranger.separate).to eq [".large", "My First Document", ".normal", ".paragraph", "This is my", ".italic", ".bold", "very first"]
   end
 end
 
   describe '#get_string_indexes' do
   it 'separates the text based on what method calls they will receive' do
     separated_text = text_arranger.separate()
-    expect(text_arranger.get_string_indexes(separated_text)).to eq [1, 4, 7]
+    expect(text_arranger.get_string_indexes).to eq [1, 4, 7]
   end
 end
 
