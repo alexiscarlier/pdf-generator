fae95c467363dba14a9dfde6d9d84245ce17d869
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-18 08:00:53 +0000

    Refactor get string indexes method and test

--- lib/text_arranger.rb
+++ lib/text_arranger.rb
@@ -8,12 +8,11 @@ class TextArranger
     @text.split(/\n+/)
   end
 
-  def get_string_indexes
-    arr = self.separate
+  def get_string_indexes(separated_text)
     i = 0
     strings_indexes = []
-    while i < arr.length
-      if arr[i][0] != '.'
+    while i < separated_text.length
+      if separated_text[i][0] != '.'
         strings_indexes.push(i)
       end
       i += 1
--- spec/text_arranger_spec.rb
+++ spec/text_arranger_spec.rb
@@ -21,8 +21,8 @@ end
 
   describe '#get_string_indexes' do
   it 'separates the text based on what method calls they will receive' do
-    text_arranger.separate()
-    expect(text_arranger.get_string_indexes()).to eq [1, 4, 7]
+    separated_text = text_arranger.separate()
+    expect(text_arranger.get_string_indexes(separated_text)).to eq [1, 4, 7]
   end
 end
 
