07e809482d720bff26344c391754988f8f1412db
Alexis Carlier <alexis.carlier0@gmail.com>
2017-12-18 09:57:06 +0000

    Add get arranged text method and test

--- lib/text_arranger.rb
+++ lib/text_arranger.rb
@@ -1,6 +1,6 @@
 class TextArranger
 
-  attr_reader :text, :separated_text
+  attr_reader :text, :separated_text, :string_indexes, :arranged_text
 
   def initialize(text)
     @text = text
@@ -19,9 +19,18 @@ class TextArranger
       end
       i += 1
     end
-    string_indexes
+    @string_indexes = string_indexes
   end
 
-
+  def get_arranged_text
+    arranged_text = []
+    arranged_text.push(@separated_text[0..@string_indexes[0]])
+    i = 0
+    while i < @string_indexes.length - 1
+      arranged_text.push(@separated_text[@string_indexes[i]+1..@string_indexes[i+1]])
+      i += 1
+    end
+    @arranged_text = arranged_text
+  end
 
 end
--- spec/text_arranger_spec.rb
+++ spec/text_arranger_spec.rb
@@ -20,10 +20,18 @@ very first'
 end
 
   describe '#get_string_indexes' do
-  it 'separates the text based on what method calls they will receive' do
-    separated_text = text_arranger.separate()
+  it 'gets the indexes for strings which will appear in the pdf' do
+    separated_text = text_arranger.separate
     expect(text_arranger.get_string_indexes).to eq [1, 4, 7]
   end
+end
+
+describe '#get_arranged_text' do
+it 'arranges the text in arrays with strings and commands' do
+  text_arranger.separate
+  text_arranger.get_string_indexes
+  expect(text_arranger.get_arranged_text).to eq [[".large", "My First Document"], [".normal", ".paragraph", "This is my"], [".italic", ".bold", "very first"]]
+end
 end
 
 end
